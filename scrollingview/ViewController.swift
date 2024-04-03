4c7e560a9bf1b084da58637114a11e33ec4a969e
keyur73 <keyurkhambhadiya73@gmail.com>
2024-04-03 15:42:04 +0530

    so i have worked with scrollview and whenever you pressed outside of texfield it will automatically hide keyboard and if you press done or retuen on keyboard keyboard will hide

--- scrollingview/ViewController.swift
+++ scrollingview/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var textField2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField1.delegate = self
+        textField2.delegate = self
+        self.hideKeyboardTappedAround()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
+extension UIViewController{
+    func hideKeyboardTappedAround (){
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer (target:self, action:#selector(UIViewController.dismissKeyboard) )
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard(){
+        view.endEditing (true)
+    }
+}
